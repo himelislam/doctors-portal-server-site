53c84212f1ab02261f9fab23e260863b3cb5c7b6
himelislam <himel7100@gmail.com>
2022-05-21 02:51:43 +0600

    payment method added

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('users');
         const doctorCollection = client.db('doctors_portal').collection('doctors');
+        const paymentCollection = client.db('doctors_portal').collection('payments');
 
 
         const verifyAdmin = async (req, res, next) => {
@@ -176,6 +177,21 @@ async function run() {
             const result = await bookingCollection.insertOne(booking)
             return res.send({ success: true, result })
         });
+
+        app.patch('/booking/:id', async (req, res)=>{
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = {_id:ObjectId(id)};
+            const updatedDoc = {
+                $set:{
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const result = await paymentCollection.insertOne(payment);
+            const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking);
+        })
 
 
         // for doctors
